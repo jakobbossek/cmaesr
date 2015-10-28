5e4751f1b1dd59dc0745b3e46f0260e1880e6e46
Jakob Bossek <info@jakobbossek.de>
2015-10-28 14:49:36 +0100

    update tests

--- tests/testthat/test_runCMAES.R
+++ tests/testthat/test_runCMAES.R
@@ -1,28 +1,30 @@
 context("CMA-ES run")
 
 test_that("CMA-ES finds optimum of sphere function", {
-	max.iters = 100L
+	max.iters = 1000L
 	population.size = 50L
 	sigma = 0.5
-	objective.fun = makeSingleObjectiveFunction(
-		name = "Sphere fun",
-		fn = function(x) sum(x^2),
-		par.set = makeParamSet(
-			makeNumericParam("x1", lower = -10, upper = 10),
-			makeNumericParam("x2", lower = -10, upper = 10)
-		)
-	)
+  dims = c(2, 3, 4, 5)
 
-	res = runCMAES(objective.fun,
-		start.point = c(7, 7),
-		population.size = population.size,
-		max.iter = max.iters,
-		sigma = sigma,
-		monitor = NULL)
+  for (dim in dims) {
+    objective.fun = makeSphereFunction(dim)
+    par.set = getParamSet(objective.fun)
+    lb = getLower(par.set)[1L]; ub = getUpper(par.set)[1L]
+    res = runCMAES(
+      objective.fun,
+      start.point = runif(dim, min = lb, max = ub),
+      population.size = population.size,
+      max.iter = max.iters,
+      sigma = sigma,
+      monitor = NULL
+    )
+    print(res$best.fitness)
 
-	expect_less_than(res$best.fitness, 0.01)
-	expect_equal(res$n.iters, max.iters)
-	expect_equal(res$convergence, 0L) # 0 for iter > max.iter
-	expect_equal(res$message, cma:::getTerminationMessage(0L))
+    expect_less_than(res$best.fitness, 0.01, info = sprintf("Desired fitness level not reached for dim = %i", dim))
+    expect_true(all(res$best.param <= 0.01), info = sprintf("Desired parameter approximation not reached for dim = %i", dim))
+    expect_equal(res$n.iters, max.iters)
+    expect_equal(res$convergence, 0L) # 0 for iter > max.iter
+    expect_equal(res$message, cma:::getTerminationMessage(0L))
+  }
 })
 

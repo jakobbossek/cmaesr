d697a1466615040597fcc2455278c4ac67ceb1fb
Jakob Bossek <info@jakobbossek.de>
2015-10-28 16:53:40 +0100

    extend test (more functions)

--- tests/testthat/test_runCMAES.R
+++ tests/testthat/test_runCMAES.R
@@ -1,30 +1,39 @@
 context("CMA-ES run")
 
 test_that("CMA-ES finds optimum of sphere function", {
-	max.iters = 1000L
+	max.iters = 50L
 	population.size = 50L
-	sigma = 0.5
+	sigma = 1.5
   dims = c(2, 3, 4, 5)
 
-  for (dim in dims) {
-    objective.fun = makeSphereFunction(dim)
-    par.set = getParamSet(objective.fun)
-    lb = getLower(par.set)[1L]; ub = getUpper(par.set)[1L]
-    res = runCMAES(
-      objective.fun,
-      start.point = runif(dim, min = lb, max = ub),
-      population.size = population.size,
-      max.iter = max.iters,
-      sigma = sigma,
-      monitor = NULL
-    )
-    print(res$best.fitness)
+  # accepted tolerance value for parameter and fitness values
+  tol = 0.05
+  fun.generators = c(makeSphereFunction, makeAckleyFunction, makeDoubleSumFunction)
 
-    expect_less_than(res$best.fitness, 0.01, info = sprintf("Desired fitness level not reached for dim = %i", dim))
-    expect_true(all(res$best.param <= 0.01), info = sprintf("Desired parameter approximation not reached for dim = %i", dim))
-    expect_equal(res$n.iters, max.iters)
-    expect_equal(res$convergence, 0L) # 0 for iter > max.iter
-    expect_equal(res$message, cma:::getTerminationMessage(0L))
-  }
+  for (generator in fun.generators) {
+    for (dim in dims) {
+      fn = do.call(generator, list(dim))
+      par.set = getParamSet(fn)
+      opt = getGlobalOptimum(fn)
+      lb = getLower(par.set)[1L]; ub = getUpper(par.set)[1L]
+
+      res = runCMAES(
+        fn,
+        start.point = runif(dim, min = lb, max = ub),
+        population.size = population.size * dim,
+        max.iter = max.iters,
+        sigma = sigma,
+        monitor = NULL
+      )
+
+      expect_true(abs(res$best.fitness - opt$value) < tol,
+        info = sprintf("Desired fitness level not reached for dim = %i and function '%s'", dim, getName(fn)))
+      expect_true(sum((res$best.param - opt$param)^2) < tol,
+        info = sprintf("Desired parameter approximation not reached for dim = %i and function '%s'", dim, getName(fn)))
+      #expect_equal(res$n.iters, max.iters)
+      #expect_equal(res$convergence, 0L) # 0 for iter > max.iter
+      #expect_equal(res$message, cma:::getTerminationMessage(0L))
+    } # dims
+  } # fun.generators
 })
 
